1cba060f33d839431a9b86eddc81d259743b24d5
101SEOguru101 <eric@youtechagency.com>
2025-02-05 12:54:29 -0600

    Update trends_api.py

--- trends_api.py
+++ trends_api.py
@@ -48,4 +48,5 @@ def get_trends():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
